0f0b87489732aa34fc5fc8d1e42870a7488e2c7a
rajoanraj <rajoanislam55@gmail.com>
2026-08-12 21:27:15 +0600

    all ciurses vercel

--- src/app/all-courses/page.jsx
+++ src/app/all-courses/page.jsx
@@ -1,8 +1,10 @@
 import Category from "@/components/Category";
 import CoursesCard from "@/components/CoursesCard";
 const AllCourses = async () => {
-const baseUrl = process.env.NODE_ENV === "development" 
-? "http://localhost:3000" :`https://${process.env.VERCEL_URL || "skillsphere-virid.vercel.app"}`;
+const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "https://skillsphere-virid.vercel.app/data.json"
+    : `https://${process.env.VERCEL_URL || "skillsphere-virid.vercel.app"}`;
   const res = await fetch(`${baseUrl}/data.json`,{ cache: 'no-store' });
   const courses = await res.json();
 
