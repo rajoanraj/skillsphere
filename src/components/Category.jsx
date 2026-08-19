cf80ea7a511b963fa38b901e420eb5cedbc96b6c
rajoanraj <rajoanislam55@gmail.com>
2026-08-19 18:41:14 +0600

    auth

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,7 +2,10 @@ import { Button } from "@heroui/react";
 import React from "react";
 
 const Category = async () => {
-const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000" : `https://${process.env.VERCEL_URL || "skillsphere-virid.vercel.app"}`;
+const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "https://skillsphere-virid.vercel.app"
+    : `https://${process.env.VERCEL_URL || "skillsphere-virid.vercel.app"}`;
   const res = await fetch(`${baseUrl}/category.json`, { cache: "no-store" }
   );
   const categories = await res.json();
